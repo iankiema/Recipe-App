cef6c44dbaa20665de6add9fab6f1201b146e8dc
unknown <zelalemyohannes75@gmail.com>
2023-11-21 16:22:24 +0300

    add files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_113945) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_132107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_113945) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
@@ -37,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_113945) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_113945) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
